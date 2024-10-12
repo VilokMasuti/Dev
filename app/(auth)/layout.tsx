2ab52404bf8a07c526a13e41ba544e073abf2499
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-10-12 14:11:21 +0530

    Routes,Auth
    Auth done by Clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main
+      className=" flex
+     min-h-screen justify-center items-center"
+    >
+      {children}
+    </main>
+  )
+}
+
+export default Layout
