a0b8b1c0551b16987780ba9220a55f64cc1ca9b3
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-10-12 20:20:57 +0530

    Navbar
    NavbarUi is done

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import MobileNav from './MobileNav'
-// import GlobalSearch from '../search/GlobalSearch'
+import GlobalSearch from '../search/GlobalSearch'
 import Theme from '../navbar/Theme'
 const Navbar = () => {
   return (
@@ -20,7 +20,7 @@ const Navbar = () => {
           Dev <span className="text-primary-500">Overflow</span>
         </p>
       </Link>
-      {/* <GlobalSearch /> */}
+      <GlobalSearch />
       <div className="flex-between gap-5">
         <Theme />
 
