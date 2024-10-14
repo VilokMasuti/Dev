eb8f30231f8e838e3f4401000aa9f5fa1ae3819e
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-10-14 19:33:02 +0530

    Form -ask a question
    Form -ask a question

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,15 @@
+import Question from '@/components/Form/Question'
 import React from 'react'
 
 const page = () => {
-  return <div>page</div>
+  return (
+    <div>
+      <h1 className=" h1-bold  text-dark_100_light900">Ask a Question</h1>
+      <div className=" mt-9">
+        <Question />
+      </div>
+    </div>
+  )
 }
 
 export default page
---
+++ lib/validations.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+//  zod for validateing
+export const QuestionsSchema = z.object({
+  title: z.string().min(5).max(130),
+  explanation: z.string().min(50),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+})
