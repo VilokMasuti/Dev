846f464984140f628002fb1b6ade9498c1eb2e65
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-10-15 16:35:38 +0530

    RANDERTAG
    TAG ERROR

--- components/Cards/QuestionCard.tsx
+++ components/Cards/QuestionCard.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
-import RanderTag from '../shared/RanderTag'
+import { SignedIn } from '@clerk/nextjs'
+
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils'
 import Metric from '../shared/Metric'
+import RenderTag from '../shared/RanderTag'
 interface QuestionProps {
   _id: string
   title: string
@@ -22,7 +24,6 @@ interface QuestionProps {
   createdAt: Date
   clerkId?: string | null
 }
-
 const QuestionCard = ({
   _id,
   title,
@@ -46,12 +47,16 @@ const QuestionCard = ({
             </h3>
           </Link>
         </div>
+
+        <SignedIn></SignedIn>
       </div>
+
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
-          <RanderTag key={tag._id} name={tag.name} _id={tag._id} />
+          <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
       </div>
+
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
           imgUrl={author.picture}
@@ -62,28 +67,29 @@ const QuestionCard = ({
           isAuthor
           textStyles="body-medium text-dark400_light700"
         />
-
-        <Metric
-          imgUrl="/assets/icons/like.svg"
-          alt="Upvotes"
-          value={formatAndDivideNumber(upvotes.length)}
-          title=" Votes"
-          textStyles="small-medium text-dark400_light800"
-        />
-        <Metric
-          imgUrl="/assets/icons/message.svg"
-          alt="message"
-          value={formatAndDivideNumber(answers.length)}
-          title=" Answers"
-          textStyles="small-medium text-dark400_light800"
-        />
-        <Metric
-          imgUrl="/assets/icons/eye.svg"
-          alt="eye"
-          value={formatAndDivideNumber(views)}
-          title=" Views"
-          textStyles="small-medium text-dark400_light800"
-        />
+        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
+          <Metric
+            imgUrl="/assets/icons/like.svg"
+            alt="Upvotes"
+            value={formatAndDivideNumber(upvotes.length)}
+            title=" Votes"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/message.svg"
+            alt="message"
+            value={formatAndDivideNumber(answers.length)}
+            title=" Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/eye.svg"
+            alt="eye"
+            value={formatAndDivideNumber(views)}
+            title=" Views"
+            textStyles="small-medium text-dark400_light800"
+          />
+        </div>
       </div>
     </div>
   )
--- components/shared/RanderTag.tsx
+++ components/shared/RanderTag.tsx
@@ -1,29 +1,26 @@
-import React from 'react'
 import Link from 'next/link'
+import React from 'react'
 import { Badge } from '@/components/ui/badge'
-interface Tags {
+
+interface Props {
   _id: string
   name: string
+  totalQuestions?: number
   showCount?: boolean
-  totalQuestions: number
 }
-const RanderTag = ({ name, _id, totalQuestions, showCount }: Tags) => {
-  return (
-    <section>
-      <Link
-        href={`/tags/${_id}`}
-        className="flex justify-between items-center gap-2"
-      >
-        <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
-          {name}
-        </Badge>
 
-        {showCount && (
-          <p className="small-medium text-dark500_light700">{totalQuestions}</p>
-        )}
-      </Link>
-    </section>
+const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
+  return (
+    <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+        {name}
+      </Badge>
+
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
+      )}
+    </Link>
   )
 }
 
-export default RanderTag
+export default RenderTag
